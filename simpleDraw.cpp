9dab0a786ee298c826d346435e15f305dd05e881
modyehab3 <ahmedehabsaoud333@yahoo.com>
2020-04-03 00:29:54 -0300

    Implemented quick sort. Needs polishing.

--- simpleDraw.cpp
+++ simpleDraw.cpp
@@ -2,7 +2,7 @@
 #include <time.h>
 
 #define Y_LIMIT 120
-#define ANIMATION_SPEED 17
+#define ANIMATION_SPEED 20
 #define MAX_ANIMATION_SPEED 20
 #define HIGHLIGHT_COLOR 0xFFE0
 
@@ -27,8 +27,10 @@ short color[10] = {0xFFFF, 0xF800, 0x07E0, 0x001F, 0x3478, 0xEAFF, 0x07FF, 0xF81
 void clear_screen();
 void draw_rectangle(int xPos, int yPos, int height, short int box_color);
 void animate_swap(int xPos1, int xPos2, int height1, int height2,  short int box_color1, short int box_color2);
+void animate_double_swap(int xPos1, int xPos2, int height1, int height2,  short int box_color1, short int box_color2);
 void swap(int* x, int* y);
 void bubble_sort(int a[], int size);
+void quick_sort(int a[], int low, int high);
 void wait_for_vsync();
 
 void initialize();
@@ -82,7 +84,8 @@ int main(void)
     wait_for_vsync();
     pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
 
-    bubble_sort(numbers, N);
+    //bubble_sort(numbers, N);
+    quick_sort(numbers, 0, N-1);
 
     
 
@@ -166,7 +169,7 @@ void bubble_sort(int a[], int size) {
             if (a[j] > a[j+1]) {
 
                 //Animate the swap of the rectangles
-                animate_swap(xPos1,xPos2,a[j], a[j+1] ,color[1], color[1]);
+                animate_double_swap(xPos1,xPos2,a[j], a[j+1] ,color[1], color[1]);
 
                 //Unhighlight the rectangles
                 draw_rectangle(xPos1,0, a[j+1], color[1]);
@@ -359,4 +362,196 @@ void draw_array(){
     for(int i=0; i < N; i++){
         draw_rectangle(INITIAL + SPACE*i, 0, numbers[i], color[1]); 
     }
+}
+
+
+
+/**********************************************************************************************************************
+ * Takes in info of rect 1 (should be located on the left) and rect 2 (should be located on the right).
+ * The animation is as follows:
+ * Rect 1 and 2 will be pushed up
+ * Rect 1 will be pushed to the right in place of rect 2, and rect 2 will be pushed left in place of rect 1
+ * Rect 1 and 2 will be lowered back down
+ * *******************************************************************************************************************/
+void animate_double_swap(int xPos1, int xPos2, int height1, int height2,  short int box_color1, short int box_color2){
+
+    //Lifting Rect 1 and 2 up
+    int startY = 0;
+    int y;
+    for (y = startY; y <= Y_LIMIT; y += ANIMATION_SPEED){
+
+        //Erase previous rectangle
+        if (y >= 2*ANIMATION_SPEED){
+            draw_rectangle(xPos1, y - 2*ANIMATION_SPEED, height1, 0x0000); //Note that previous rectangle on the swapped buffer was two positions behind current position
+            draw_rectangle(xPos2, y - 2*ANIMATION_SPEED, height2, 0x0000); //Note that previous rectangle on the swapped buffer was two positions behind current position
+        }
+        else{
+            draw_rectangle(xPos1, startY, height1, 0x0000); 
+            draw_rectangle(xPos2, startY, height2, 0x0000); 
+        }
+
+        //Draw new rectangle
+        draw_rectangle(xPos1, y, height1, HIGHLIGHT_COLOR);
+        draw_rectangle(xPos2, y, height2, HIGHLIGHT_COLOR);
+
+        //swap buffers
+        wait_for_vsync();
+        pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+
+        //Update location
+        //done using the for loop
+    }
+
+    y = y-ANIMATION_SPEED; //store the last position of rect 2
+    //Make sure image is aligned in the two buffers
+    draw_rectangle(xPos1, y - ANIMATION_SPEED, height1, 0x0000);
+    draw_rectangle(xPos2, y - ANIMATION_SPEED, height2, 0x0000);
+    draw_rectangle(xPos1, Y_LIMIT, height1, HIGHLIGHT_COLOR);
+    draw_rectangle(xPos2, Y_LIMIT, height2, HIGHLIGHT_COLOR);
+    wait_for_vsync();
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+    draw_rectangle(xPos1, y, height1, 0x0000);
+    draw_rectangle(xPos2, y, height2, 0x0000);
+    y = Y_LIMIT;
+    draw_rectangle(xPos1, y, height1, HIGHLIGHT_COLOR);
+    draw_rectangle(xPos2, y, height2, HIGHLIGHT_COLOR);
+
+
+    int x1, x2;
+    //Move Rect 2 to xPos 1 and rect 1 to xPos 2
+    for (x2 = xPos2, x1 = xPos1; x2 >= xPos1 && x1 <= xPos2; x2 -= ANIMATION_SPEED, x1 += ANIMATION_SPEED){
+
+        //Erase previous rectangles
+        if (x2 <= xPos2 - 2*ANIMATION_SPEED){
+            draw_rectangle(x2 + 2*ANIMATION_SPEED, y, height2, 0x0000); //Note that previous rectangle on the swapped buffer was two positions behind current position        
+        }
+        else{
+            draw_rectangle(xPos2, y, height2, 0x0000);
+        }
+
+        if (x1 >= xPos1 + 2*ANIMATION_SPEED){
+            draw_rectangle(x1 - 2*ANIMATION_SPEED, y, height1, 0x0000); //Note that previous rectangle on the swapped buffer was two positions behind current position        
+        }
+        else{
+            draw_rectangle(xPos1, y, height1, 0x0000);
+        }
+        
+
+        //Draw new rectangle
+        draw_rectangle(x2, y, height2, HIGHLIGHT_COLOR);
+        draw_rectangle(x1, y, height1, HIGHLIGHT_COLOR);
+
+        //swap buffers
+        wait_for_vsync();
+        pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+
+        //Update location
+        //done using the for loop
+
+    }
+    x2 = x2 + ANIMATION_SPEED;
+    x1 = x1 - ANIMATION_SPEED;
+    draw_rectangle(x2 + ANIMATION_SPEED, y, height2, 0x0000);
+    draw_rectangle(x1 - ANIMATION_SPEED, y, height1, 0x0000);
+    draw_rectangle(xPos1, y, height2, HIGHLIGHT_COLOR);
+    draw_rectangle(xPos2, y, height1, HIGHLIGHT_COLOR);
+    wait_for_vsync();
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+    draw_rectangle(x2, y, height2, 0x0000);
+    draw_rectangle(x1, y, height1, 0x0000);
+    x2 = xPos1;
+    x1 = xPos2;
+    draw_rectangle(x2, y, height2, HIGHLIGHT_COLOR);
+    draw_rectangle(x1, y, height1, HIGHLIGHT_COLOR);
+
+    //Move Rect 1 & 2 back down
+    for (y = Y_LIMIT; y >= 0; y -= ANIMATION_SPEED){
+
+        //Erase previous rectangle
+        if (y <= Y_LIMIT - 2*ANIMATION_SPEED){
+            draw_rectangle(x2, y + 2*ANIMATION_SPEED, height2, 0x0000); //Note that previous rectangle on the swapped buffer was two positions behind current position        
+            draw_rectangle(x1, y + 2*ANIMATION_SPEED, height1, 0x0000); //Note that previous rectangle on the swapped buffer was two positions behind current position        
+        }
+
+        else{
+            draw_rectangle(x2, Y_LIMIT, height2, 0x0000);
+            draw_rectangle(x1, Y_LIMIT, height1, 0x0000);
+        }
+        
+
+        //Draw new rectangle
+        draw_rectangle(x2, y, height2, HIGHLIGHT_COLOR);
+        draw_rectangle(x1, y, height1, HIGHLIGHT_COLOR);
+
+        //swap buffers
+        wait_for_vsync();
+        pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+
+        //Update location
+        //done using the for loop
+
+    }
+    y = y + ANIMATION_SPEED;
+    draw_rectangle(x2, y + ANIMATION_SPEED, height2, 0x0000);
+    draw_rectangle(x1, y + ANIMATION_SPEED, height1, 0x0000);
+    draw_rectangle(x2, 0, height2, HIGHLIGHT_COLOR);
+    draw_rectangle(x1, 0, height1, HIGHLIGHT_COLOR);
+    wait_for_vsync();
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+    draw_rectangle(x2, y, height2, 0x0000);
+    draw_rectangle(x1, y, height1, 0x0000);
+    y = 0;
+    draw_rectangle(x2, y, height2, HIGHLIGHT_COLOR);
+    draw_rectangle(x1, y, height1, HIGHLIGHT_COLOR);
+
+
+}
+
+
+/* This function takes last element as pivot, places 
+   the pivot element at its correct position in sorted 
+    array, and places all smaller (smaller than pivot) 
+   to left of pivot and all greater elements to right 
+   of pivot */
+int partition (int a[], int low, int high) 
+{ 
+    int pivot = a[high];    // pivot 
+    int i = (low - 1);  // Index of smaller element 
+  
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        // If current element is smaller than the pivot 
+        if (a[j] < pivot) 
+        { 
+            i++;    // increment index of smaller element 
+            //Animate the swap of the rectangles
+            int xPos1 = INITIAL + SPACE*i;
+            int xPos2 = INITIAL + SPACE*j;
+            animate_double_swap(xPos1,xPos2,a[i], a[j] ,color[1], color[1]);
+            swap(&a[i], &a[j]); 
+        } 
+    } 
+
+    //Animate the swap of the rectangles
+    int xPos1 = INITIAL + SPACE*(i+1);
+    int xPos2 = INITIAL + SPACE*(high);
+    animate_double_swap(xPos1,xPos2,a[i + 1], a[high] ,color[1], color[1]);
+    swap(&a[i + 1], &a[high]); 
+    return (i + 1); 
+} 
+  
+
+void quick_sort(int a[], int low, int high) 
+{ 
+    if (low < high) 
+    { 
+        /* pi is partitioning index, arr[p] is now 
+           at right place */
+        int pi = partition(a, low, high); 
+  
+        // Separately sort elements before 
+        // partition and after partition 
+        quick_sort(a, low, pi - 1); 
+        quick_sort(a, pi + 1, high); 
+    } 
 }
